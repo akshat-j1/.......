85bfc5bec0159186bdf600c34652a4512fddabd9
Akshat Jain <akshatjain@Akshats-MacBook-Air.local>
2026-06-15 01:05:17 +0530

    fix: match exit with or without arguments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,7 +13,7 @@ public class Main {
             
             String input = scanner.nextLine().trim();
             
-            if (input.equals("exit 0")) {
+            if (input.equals("exit") || input.startsWith("exit ")) {
                 System.exit(0);
             } else {
                 System.out.println(input + ": command not found");
