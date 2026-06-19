7cb49329741496c74b750df49da4d450e3c269a8
Akshat Jain <akshatjain9021@gmail.com>
2026-06-19 16:22:52 +0530

    feat: implement double quoting string parsing support

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -92,15 +92,19 @@ public class Main {
         List<String> args = new ArrayList<>();
         StringBuilder currentArg = new StringBuilder();
         boolean inSingleQuotes = false;
+        boolean inDoubleQuotes = false;
         boolean hasContent = false;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
 
-            if (c == '\'') {
+            if (c == '\'' && !inDoubleQuotes) {
                 inSingleQuotes = !inSingleQuotes;
                 hasContent = true;
-            } else if (Character.isWhitespace(c) && !inSingleQuotes) {
+            } else if (c == '"' && !inSingleQuotes) {
+                inDoubleQuotes = !inDoubleQuotes;
+                hasContent = true;
+            } else if (Character.isWhitespace(c) && !inSingleQuotes && !inDoubleQuotes) {
                 if (currentArg.length() > 0 || hasContent) {
                     args.add(currentArg.toString());
                     currentArg.setLength(0);
